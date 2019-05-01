981af685d410e4dc248db781dfa8131a22f2ecb5
LimSujin0 <imsujin0@naver.com>
2019-05-02 01:00:12 +0900

    read and print node

--- hw4_1.c
+++ hw4_1.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE 10
+#define BUFSIZE 7
+#define STRSIZE 100
 
 struct Node {
 	char data;
@@ -13,67 +14,46 @@ void print_node(struct Node* nodes);
 
 int main() {
 	//a = 65, g = 71
-	int row_index = 0, col_index=0;
+	int row_index = 0, col_index = 0;
 	char* strcut;
-	struct Node * nodes = (struct Node*)malloc(sizeof(struct Node)*SIZE);
+	struct Node * nodes = (struct Node*)malloc(sizeof(struct Node)*BUFSIZE);
 	struct Node * node_p;
 	char* str;
-	str = (char*)malloc(sizeof(char)*SIZE);
+	str = (char*)malloc(sizeof(char)*BUFSIZE);
 
 	FILE *fp;
-	fp = fopen("hw4.txt", "r");
-	if (fp == NULL)
-	{
-		perror("file open error");
-		system("pause");
-		exit(0);
-	}
+	fp = fopen("hw4.data", "r");
 
-	fgets(str, SIZE, fp); //delete first line
-	while(fgets(str, SIZE, fp)!=NULL) {
+	//make Adj array and adjacency list in alphabetical order.
+	fgets(str, STRSIZE, fp); //delete first line
+	while (fgets(str, STRSIZE, fp) != NULL) {
 		printf("%s", str);
 
-		//strcut = strtok(str, "\t");//get first column char
+		strcut = strtok(str, "\t");
 		row_index = (*strcut) - 65; //get index
 		nodes[row_index].data = (*strcut); //make first node
 		nodes[row_index].next = NULL;
-		
-		col_index = 0;
+
+		col_index = 0; //initalize for column
 		node_p = &(nodes[row_index]);
 
-		strcut = strtok(str, "\t");
 		strcut = strtok(NULL, "\t");
-		while (strcut != NULL) { //add node
-			
+		while (strcut != NULL) {
 			int exist = (*strcut) - 48; //change asccii code to integer
+			//printf("%d\n", exist);
 			if (exist == 1) {
-				printf("%c\n", strcut);
 				struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
 				new_node->data = col_index + 65;
 				new_node->next = NULL;
 				node_p->next = new_node;
-				//printf("row index : %d, col index : %d, new data:%d\n", row_index, col_index, node_p->next->data);
 				node_p = new_node;
 			}
-			
-			printf("%c\n", strcut);
+			col_index++;
 			strcut = strtok(NULL, "\t");
-			//col_index++;
 		}
-		
 	}
+	print_node(nodes);
 
-	//print_node(nodes);
-
-	/*
-	printf("\n");
-	struct Node * nodep = (struct Node*)malloc(sizeof(struct Node));
-	nodep = (&nodes[0]);
-	while(nodep!=NULL){	
-		printf("%c\n", nodep->data);
-		nodep = nodep->next;
-	}
-	*/
 
 	fclose(fp);
 	system("pause");
@@ -81,8 +61,8 @@ int main() {
 }
 
 void print_node(struct Node* nodes) {
-	printf("****print graph****");
-	for (int i = 0; i < SIZE; i++) {
+	printf("\n\n****print graph****\n");
+	for (int i = 0; i < BUFSIZE; i++) {
 		struct Node* node = (&nodes[i]);
 		while (node != NULL) {
 			printf("%c -> ", node->data);
